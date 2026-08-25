d6b07858e282f9b00fd11f7521926fbc4228c637
Rob77Af <flauzino27@gmail.com>
2026-08-25 12:06:09 -0300

    feat: initialize app layout with global context provider and base landing page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+"use client";
+
 import Image from "next/image";
+import { useGlobalContext } from "../../global-context";
 
 export default function Home() {
+  const { locale } = useGlobalContext();
+
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <div className="mb-4 font-semibold text-black dark:text-white">
+          Current Language: {locale?.name}
+        </div>
         <Image
           className="dark:invert h-5 w-[100px]"
           src="/next.svg"
